c8a100147be4d170716128e4b6c15a51f01f685b
Joseba <joseba.merino.pina@gmail.com>
2020-02-21 09:16:37 +0100

    AÑadido formulario y navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PrincipalComponent } from './paginas/principal/principal.component';
 import { DetalleComponent } from './paginas/detalle/detalle.component';
+import { FormularioComponent } from './paginas/formulario/formulario.component';
 
 
 const routes: Routes = [
+  { path: 'formulario', component: FormularioComponent },
   { path: 'detalle/:id', component: DetalleComponent },
   { path: '', component: PrincipalComponent },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrincipalComponent } from './paginas/principal/principal.component';
 import { DetalleComponent } from './paginas/detalle/detalle.component';
+import { FormularioComponent } from './paginas/formulario/formulario.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrincipalComponent,
-    DetalleComponent
+    DetalleComponent,
+    FormularioComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
