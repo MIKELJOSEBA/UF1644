3f233a65eda7d8e310d495252f8e224de1d4079a
Joseba <joseba.merino.pina@gmail.com>
2020-02-21 08:45:22 +0100

    Escructura principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PrincipalComponent } from './paginas/principal/principal.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PrincipalComponent },
+];
+
+export const RUTAS = [
+  {
+    url: '/',
+    nombre: 'inicio'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
